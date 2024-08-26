8fef5b2409b2ed4f9856d5f65b8febb7899dd012
Manvendra Shekhawat <126936011+Black0Panther@users.noreply.github.com>
2024-08-26 13:08:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+ }
+
+ function appendNumber(number) {
+    const display = document.getElementById('display');
+    display.value += number;
+ }
+
+ function appendOperator(operator) {
+    const display = document.getElementById('display');
+    display.value += operator;
+ }
+
+ function calculate() {
+    const display = document.getElementById('display');
+    try {
+       display.value = eval(display.value);
+    } catch (e) {
+       display.value = 'Error';
+    }
+ }
